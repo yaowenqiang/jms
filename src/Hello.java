dd451b446ca7ec86604d3a57871064491458828b
yaowenqiang <yaowenqiang111@163.com>
2020-01-31 16:52:21 +0800

    add String demo

--- src/Hello.java
+++ src/Hello.java
@@ -99,6 +99,24 @@ public class Hello {
         System.out.println(myBooleanVal);
         System.out.println(isMale);
 
+        String myString = "This is a string";
+        System.out.println(myString);
+        myString = myString + " and more";
+        System.out.println(myString);
+
+        myString = myString + " " + copyright + "2020";
+        System.out.println(myString);
+
+        String numberString = "250.55";
+        numberString = numberString + "49.45";
+        System.out.println(numberString);
+
+        String lastString = "10";
+        int myInt =  20;
+        lastString = lastString + myInt;
+        System.out.println(lastString);
+
+
 
 
 
