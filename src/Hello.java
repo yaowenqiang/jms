a5043e3ae46f7869e48d3972929e745879c901a7
yaowenqiang <yaowenqiang111@163.com>
2020-02-01 17:31:38 +0800

    add for loop demo

--- src/Hello.java
+++ src/Hello.java
@@ -161,6 +161,7 @@ public class Hello {
 
         int result2 = calculateScore(true, 100, 1, 10);
         switchCase();
+        forLoop();
     }
 
 
@@ -228,6 +229,25 @@ public class Hello {
                 break;
         }
 
+    }
+
+    public static void forLoop() {
+        for(int i = 0; i < 100; i++) {
+            System.out.println("i equal to : " + i);
+        }
+    }
+
+    public static boolean isPrime(int n) {
+        if (n == 1) {
+            return false;
+        }
+
+        for (int i = 2; i <= n/2; i++) {
+            if (n % i == 0) {
+                return false;
+            }
+        }
+        return true;
     }
 
 }
