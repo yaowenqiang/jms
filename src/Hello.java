97935f6ffa34668a049adf8bad35f0dea5a017f1
yaowenqiang <yaowenqiang111@163.com>
2020-02-02 23:00:07 +0800

    add oop master challenge part 1

--- src/Hello.java
+++ src/Hello.java
@@ -231,6 +231,14 @@ public class Hello {
                     "Plot: " + movie.plot() + "\n");
         }
 
+
+        Hamburger hamburger = new Hamburger("Basic", "Sausage", 3.56, "White");
+        double price = hamburger.itemizeHamburger();
+        hamburger.addHamburgerAddition1("Tomato", 0.27);
+        hamburger.addHamburgerAddition2("Lettuce", 0.75);
+        hamburger.addHamburgerAddition3("Cheese", 1.12);
+        price = hamburger.itemizeHamburger();
+        System.out.println("Total burger price is " + price);
     }
 
 
