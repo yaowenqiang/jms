8b30e6098b3f8edd7fb61374a721957aa02e468b
yaowenqiang <yaowenqiang111@163.com>
2020-02-04 21:27:41 +0800

    add sortIntegers method jj# Please enter the commit message for your changes. Lines starting

--- src/ArrayDemo.java
+++ src/ArrayDemo.java
@@ -58,4 +58,29 @@ public class ArrayDemo {
     }
 
 
+    public static int[] sortIntegers(int[] array) {
+        int[] sortedArray = new int[array.length);
+        for (int i = 0; i < array.length;i++) {
+            sortedArray[i] = array[i];
+        }
+
+        boolean flag = true;
+        int temp;
+
+        while(flag) {
+           flag = false;
+
+           for(int i = 0; i < sortedArray.length -1;i++) {
+               if(sortedArray[i] <= sortedArray[i+1]) {
+                   temp = sortedArray[i];
+                   sortedArray[i] = sortedArray[i+1];
+                   sortedArray[i+1] = temp;
+                   flag = true
+
+               }
+           }
+        }
+        return sortedArray;
+    }
+
 }
