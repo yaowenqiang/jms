cbca104730ea4a9410a57c2a2f61712acaa9430c
yaowenqiang <yaowenqiang111@163.com>
2020-02-14 02:54:14 +0800

    add local class and anonymous class demo

---
+++ src/Button.java
@@ -0,0 +1,23 @@
+public class Button {
+    private String title;
+    private OnClickListener onClickListener;
+
+    public Button(String title) {
+        this.title = title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setOnClickListener(OnClickListener onClickListener) {
+        this.onClickListener = onClickListener;
+    }
+    public void onClick() {
+        this.onClickListener.onClick(this.title);
+    }
+
+    public interface OnClickListener {
+        public void onClick(String title);
+    }
+}
--- src/InterfaceDemo.java
+++ src/InterfaceDemo.java
@@ -1,4 +1,9 @@
+import java.util.Scanner;
+
 public class InterfaceDemo {
+    private static Scanner scanner = new Scanner(System.in);
+    private static Button btnPrint = new Button("Print");
+    private static Button btnPrint2 = new Button("Print");
     public static void main(String[] args) {
         ITelephone timesPhone;
 //        DeskPhone timesPhone;
@@ -35,5 +40,42 @@ public class InterfaceDemo {
 
 
 //        System.out.println(first.driveSpeed(1000));
+
+        class ClickListener implements Button.OnClickListener {
+            public ClickListener() {
+                System.out.println("I've been attached.");
+            }
+
+            @Override
+            public void onClick(String title) {
+                System.out.println(title + " was clicked.");
+            }
+
+        }
+        btnPrint.setOnClickListener(new ClickListener());
+        btnPrint2.setOnClickListener(new Button.OnClickListener() {
+            @Override
+            public void onClick(String title) {
+                System.out.println("the click event was handled by the anonymous class,the new title is " + title);
+            }
+        });
+        listen();
+    }
+
+    private static void listen() {
+        boolean quit = false;
+        while(!quit) {
+            int choice = scanner.nextInt();
+            scanner.nextLine();
+            switch (choice) {
+                case 0:
+                    quit = true;
+                    break;
+                case 1:
+                    //btnPrint.onClick();
+                    btnPrint2.onClick();
+            }
+
+        }
     }
 }
