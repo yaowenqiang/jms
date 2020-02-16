81c3b091d62bdd315b0a843a9ae23b8dbb96f414
yaowenqiang <yaowenqiang111@163.com>
2020-02-16 20:47:16 +0800

    add visibility demo

--- src/ScopeCheck.java
+++ src/ScopeCheck.java
@@ -1,36 +1,43 @@
 public class ScopeCheck {
     public int publicVar = 0;
-    private int privateVar = 1;
+    private int varOne = 1;
 
     public ScopeCheck() {
-        System.out.println("ScopeCheck created, publicVar." + publicVar + ", privateVar: " + privateVar);
+        System.out.println("ScopeCheck created, publicVar." + publicVar + ", privateVar: " + varOne);
     }
 
-    public int getPrivateVar() {
-        return privateVar;
+    public int getVarOne() {
+        return varOne;
     }
 
     public void timesTwo() {
-        int privateVar = 2;
+        int varTwo = 2;
         for (int i = 0; i < 10; i ++) {
 //            System.out.println(i + "  times two is " + i * privateVar);
-            System.out.println(i + "  times two is " + i * this.privateVar);
+            System.out.println(i + "  times two is " + i * varTwo);
         }
 
     }
 
+    public void  useInner() {
+        InnerClass innerClass = new InnerClass();
+        System.out.println("varThree from outer class: " + innerClass.varThree);
+    }
+
     public class InnerClass {
-        public int privateVar = 3;
+//        public int varThree = 3;
+        private int varThree = 3;
 
         public InnerClass() {
-            System.out.println("InnerClass is created, privateVar is " + privateVar);
+            System.out.println("InnerClass is created, varOne is " + varOne + " and varThree is " + varThree);
         }
 
         public void timesTwo() {
 //            ScopeCheck.this.timesTwo();
+            System.out.println("varOne is still available here. " + varOne);
             for (int i = 0; i < 10; i ++) {
-//                System.out.println(i + "  times two is " + i * privateVar);
-                System.out.println(i + "  times two is " + i * ScopeCheck.this.privateVar);
+                System.out.println(i + "  times two is " + i * varThree);
+//                System.out.println(i + "  times two is " + i * ScopeCheck.this.varOne);
             }
 
         }
--- src/ScopeDemo.java
+++ src/ScopeDemo.java
@@ -1,13 +1,20 @@
 public class ScopeDemo {
     public static void main(String[] args) {
-        String privateVar = "This is private to main()";
+        String varFour = "This is private to main()";
         ScopeCheck scopeInstance = new ScopeCheck();
-        System.out.println("scopeInstance privateVar is " + scopeInstance.getPrivateVar());
-        System.out.println(privateVar);
+        System.out.println("scopeInstance varOne is " + scopeInstance.getVarOne());
+        System.out.println(varFour);
         scopeInstance.timesTwo();
 
         ScopeCheck.InnerClass innerClass = scopeInstance.new InnerClass();
         innerClass.timesTwo();
+
+        scopeInstance.useInner();
+
+        ScopeCheck.InnerClass innerClass2 = scopeInstance.new InnerClass();
+//        System.out.println("varThree is not accessable here "+ innerClass2.varThree);
+
+
 
     }
 }
