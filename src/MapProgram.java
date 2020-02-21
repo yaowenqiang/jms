0b493f4f12d1b8b7dcc33185ea9b99b4b00a4f23
yaowenqiang <yaowenqiang111@163.com>
2020-02-21 21:22:17 +0800

    add more Map demo,

--- src/MapProgram.java
+++ src/MapProgram.java
@@ -11,6 +11,16 @@ public class MapProgram {
         }
         System.out.println("Java add successfully.");
 
+        languages.put("Python", "a interpreted object-oriented, high-level programming language with dynamic semantics.");
+        languages.put("Algol", "an algorithmic language");
+        languages.putIfAbsent("Algol", "an     algorithmic language");
+        System.out.println("old value: " + languages.put("Algol", "an algorithmic language"));
+
+
+        languages.put("basic", "Beginners All purposes symbolic Instruction  Code. ");
+        languages.put("lisp", "Therein lies madness.");
+        System.out.println(languages.get("Java"));
+
         for (String key: languages.keySet()) {
             System.out.println(key + ": " + languages.get(key));
         }
