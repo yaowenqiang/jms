d7fbff00cc2151c4fa42c15cc1c7f37744c660ef
yaowenqiang <yaowenqiang111@163.com>
2020-03-05 23:10:51 +0800

    add interrupt and join  demo

--- src/AnotherThread.java
+++ src/AnotherThread.java
@@ -1,6 +1,17 @@
 public class AnotherThread extends Thread {
     @Override
     public void run() {
-        System.out.println(ThreadColor.ANSI_BLUE + "hello from another thread.");
+        //System.out.println(ThreadColor.ANSI_BLUE + "hello from another thread.");
+        System.out.println(ThreadColor.ANSI_BLUE + "hello from ." + currentThread().getName());
+        try {
+            Thread.sleep(5000);
+//            Thread.sleep();
+        } catch (InterruptedException e) {
+            System.out.println(ThreadColor.ANSI_BLUE + "another thread wake me up");
+            return;
+        }
+        System.out.println(ThreadColor.ANSI_BLUE + "Three seconds have passed and I'am awake.");
+
+
     }
 }
--- src/ThreadsProgram.java
+++ src/ThreadsProgram.java
@@ -1,8 +1,12 @@
 public class ThreadsProgram {
+    //https://docs.oracle.com/javase/7/docs/api/java/lang/Thread.html
     public static void main(String[] args) {
         System.out.println(ThreadColor.ANSI_PURPLE + "hello from the main thread.");
         Thread anotherThread = new AnotherThread();
+        anotherThread.setName("Another Thread = ");
         anotherThread.start();
+
+//        anotherThread.run();
 
         new Thread() {
             public void run() {
@@ -15,9 +19,17 @@ public class ThreadsProgram {
             @Override
             public void run() {
                 System.out.println(ThreadColor.ANSI_RED + "welcome to anonymous thread.");
+                try {
+                    anotherThread.join(2000);
+                    //anotherThread.join();
+                    System.out.println(ThreadColor.ANSI_RED + "Another thread terminated or timed out, so I'am running again.");
+                } catch (InterruptedException e) {
+                    System.out.println(ThreadColor.ANSI_RED + "I couldn't wait after all, I was interrupted. ");
+                }
             }
         });
         myRunnableThread.start();
+//        anotherThread.interrupt();
 
 
 
