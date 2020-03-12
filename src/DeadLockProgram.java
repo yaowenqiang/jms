e9b982fe874e4dc48fa53ed2876c79df41097601
yaowenqiang <yaowenqiang111@163.com>
2020-03-12 23:48:05 +0800

    fix the deadlock

--- src/DeadLockProgram.java
+++ src/DeadLockProgram.java
@@ -32,20 +32,20 @@ public class DeadLockProgram {
     {
         @Override
         public void run() {
-            synchronized (lock2) {
-                System.out.println("Thread 2: has lock2");
+            synchronized (lock1) {
+                System.out.println("Thread 2: has lock1");
                 try {
                     Thread.sleep(100);
                 } catch (InterruptedException e) {
 
                 }
-                System.out.println("thread 2 : waiting for lock 1");
-                synchronized (lock1) {
+                System.out.println("thread 2 : waiting for lock 2");
+                synchronized (lock2) {
                     System.out.println("Thread 2: has lock2 and lock1");
                 }
-                System.out.println("Thread 2 released lock1");
+                System.out.println("Thread 2 released lock2");
             }
-            System.out.println("thread 2 released lock2, exiting. ");
+            System.out.println("thread 2 released lock1, exiting. ");
         }
     }
 }
